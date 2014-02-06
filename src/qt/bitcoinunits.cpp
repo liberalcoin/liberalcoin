80fa6f58f1ac0c991e63dacb7b5494b25eaa2711
mytasks <platypusnetwork@gmail.com>
2014-02-05 19:38:18 -0500

    hfjea
    ghfldkhlk

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("ULC");
+    case mBTC: return QString("mULC");
+    case uBTC: return QString::fromUtf8("μULC");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Liberalcoins");
+    case mBTC: return QString("Milli-Liberalcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Liberalcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
